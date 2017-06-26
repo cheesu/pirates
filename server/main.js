6141aa08d4573a8ed51c4925f4280e26bd2ee485
cheesu <dlscltn@gmail.com>
2017-06-26 11:54:37 +0900

    test

--- server/main.js
+++ server/main.js
@@ -135,10 +135,45 @@ io.on('connection', (socket) => {
 
 
     socket.on('Gchat', function(msg){
+
+      msg.replace( /섹스/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+      msg.replace( /sex/gi, "전 여자랑 자볼일이 앞으로도 없는 개 찐따라라서  음담패설로 자위하는 쓰레기 찌끄레기 입니다.");
+      msg.replace( /세엑스/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+      msg.replace( /아흥/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+      msg.replace( /앗흥/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+
+      if(msg.indexOf('섹') != -1){
+          msg.replace( /섹/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+        if(msg.indexOf('스') != -1){
+
+            msg.replace( /스/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+        }
+      }
+
+      if(msg.toLowerCase().indexOf('s') != -1){
+        if(msg.toLowerCase().indexOf('e') != -1){
+          if(msg.toLowerCase().indexOf('x') != -1){
+            msg.replace( /s/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+            msg.replace( /S/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+          }
+        }
+      }
+
       io.emit('Gchat', msg);
     });
     socket.on('NoticeChat', function(msg){
-      io.emit('NoticeChat', msg);
+
+      for(var count=0; count<chatUserList.length; count++){
+        console.log(chatUserList[count].socketID);
+        console.log(chatUserList[count].userID);
+        if(chatUserList[count].socketID==socket.id){
+          if(chatUserList[count].userID=='undefined [운영자]'){
+            io.emit('NoticeChat', msg);
+          }
+        }
+      }
+
+
     });
 
 
@@ -148,6 +183,27 @@ io.on('connection', (socket) => {
     });
 
     socket.on('chat', function(msg){
+      msg.replace( /섹스/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+      msg.replace( /sex/gi, "전 여자랑 자볼일이 앞으로도 없는 개 찐따라라서  음담패설로 자위하는 쓰레기 찌끄레기 입니다.");
+      msg.replace( /세엑스/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+      msg.replace( /아흥/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+      msg.replace( /앗흥/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+
+      if(msg.indexOf('섹') != -1){
+        if(msg.indexOf('스') != -1){
+            msg.replace( /섹/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+            msg.replace( /스/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+        }
+      }
+
+      if(msg.toLowerCase().indexOf('s') != -1){
+        if(msg.toLowerCase().indexOf('e') != -1){
+          if(msg.toLowerCase().indexOf('x') != -1){
+            msg.replace( /s/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+            msg.replace( /S/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+          }
+        }
+      }
       let ch = msg.split(":ch:");
       io.emit(ch[0], ch[1]);
     });
@@ -179,6 +235,28 @@ io.on('connection', (socket) => {
 
     //귓말
     socket.on('whisper', function(wObj){
+      wObj.msg.replace( /섹스/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+      wObj.msg.replace( /sex/gi, "전 여자랑 자볼일이 앞으로도 없는 개 찐따라라서  음담패설로 자위하는 쓰레기 찌끄레기 입니다.");
+      wObj.msg.replace( /세엑스/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+      wObj.msg.replace( /아흥/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+      wObj.msg.replace( /앗흥/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+
+      if(msg.indexOf('섹') != -1){
+        if(msg.indexOf('스') != -1){
+            msg.replace( /섹/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+            msg.replace( /스/gi, "실제로 여자 신음소리 한번도 못들어본 찐따 입니다..");
+        }
+      }
+
+      if(msg.toLowerCase().indexOf('s') != -1){
+        if(msg.toLowerCase().indexOf('e') != -1){
+          if(msg.toLowerCase().indexOf('x') != -1){
+            msg.replace( /s/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+            msg.replace( /S/gi, "저는 여자 손도 못잡아본 찐따 입니다. 음담패설로 자위하는 쓰레기 입니다.");
+          }
+        }
+      }
+
       console.log(wObj);
       let sedMsg = "[귓속말] "+ wObj.sendUser + ": "+wObj.msg;
       let sedMsg2 = "[귓속말] "+ wObj.target + ">>: "+wObj.msg;
